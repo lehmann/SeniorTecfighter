f8192eb66b32b7c59a8799969ab9b76e4067f567
Limao <Limao@192.168.1.100>
2014-07-15 21:16:26 -0300

    Retirando referências ao Neo4J

--- SeniorTecfighterStore/src/main/java/com/seniortec/sorteio/store/Papeis.java
+++
@@ -1,8 +0,0 @@
-package com.seniortec.sorteio.store;
-
-import org.neo4j.graphdb.Label;
-
-public enum Papeis implements Label {
-
-	PARTICIPANTE, NAO_SORTEADO, PALESTRA, FOTO, BRINDE;
-}
--- SeniorTecfighterStore/src/main/java/com/seniortec/sorteio/store/Relacoes.java
+++
@@ -1,8 +0,0 @@
-package com.seniortec.sorteio.store;
-
-import org.neo4j.graphdb.RelationshipType;
-
-public enum Relacoes implements RelationshipType {
-
-	POSSUI_FOTO, SORTEADO_EM, BRINDE_DO_DIA, PARTICIPANTE_DO_DIA, PARTICIPOU_EM, FOTO, FOTO_DE;
-}
